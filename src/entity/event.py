bd85579990132b66d7a9aa50045171b0754e687f
Philipp Krause <phil.fhs11@googlemail.com>
2019-06-01 14:16:16 +0200

    UPDATE: entities to new framwork and structure

--- src/entity/event.py
+++
@@ -1,9 +0,0 @@
-import peewee
-
-from src.entity.base_entity import BaseEntity
-
-
-class Event(BaseEntity):
-    name = peewee.CharField(255)
-    start = peewee.TimestampField()  # todo set default
-    trigger = peewee.IntegerField()
--- src/entity/score.py
+++ src/entity/score.py
@@ -1,11 +1,11 @@
 import peewee
 
 from src.entity.base_entity import BaseEntity
-from src.entity.event import Event
 from src.entity.user import User
+from src.entity.statistic import Statistic
 
 
 class Score(BaseEntity):
-    user = peewee.ForeignKeyField(model=User, null=False, primary_key=True)
-    event = peewee.ForeignKeyField(model=Event, null=False, primary_key=True)
-    points = peewee.IntegerField(default=0)
+    user = peewee.ForeignKeyField(model=User)
+    points = peewee.IntegerField(null=False, default=0)
+    board = peewee.ForeignKeyField(model=Statistic, null=False)
