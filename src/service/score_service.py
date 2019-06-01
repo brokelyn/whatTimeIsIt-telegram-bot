d5d970c54ce13c3245606a0a07510450165ef51b
Philipp Krause <phil.fhs11@googlemail.com>
2019-06-01 14:19:15 +0200

    REFACTOR: services to new framework

--- src/service/score_service.py
+++ src/service/score_service.py
@@ -5,21 +5,4 @@ from src.repo.score_repo import ScoreRepo
 
 
 class ScoreService:
-
-    @staticmethod
-    def increase_score(event: Event, user: User):
-        score = ScoreRepo.get_score(event, user)
-        if score:
-            score.points += 1
-        else:
-            score = ScoreService.create_score(event, user)
-            score.points = 1
-            ScoreRepo.save(score)
-
-    @staticmethod
-    def create_score(event: Event, user: User):
-        score = ScoreRepo.get_score(event, user)
-        if score:
-            score = Score(user=user, event=event)
-            ScoreRepo.save(score)
-        return score
+    pass
