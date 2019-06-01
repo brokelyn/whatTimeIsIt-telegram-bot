b808ae703223d70777e835f6441472690ab64762
Philipp Krause <phil.fhs11@googlemail.com>
2019-06-01 14:18:53 +0200

    UPDATE: repos to new structure

--- src/repo/score_repo.py
+++ src/repo/score_repo.py
@@ -1,14 +1,19 @@
-from src.entity.event import Event
 from src.entity.score import Score
 from src.entity.user import User
+from src.entity.statistic import Statistic
+from typing import List
 
 
 class ScoreRepo:
 
     @staticmethod
-    def get_score(event: Event, user: User) -> Score:
-        return Score.get_or_none(Score.event == event and Score.user == user)
+    def scores_by_user(user: User) -> List[Score]:
+        return list(Score.select().where(Score.user == user))
 
     @staticmethod
     def save(score: Score):
         score.save()
+
+    @staticmethod
+    def scores_to_scoreboard(scoreboard: Statistic) -> List[Score]:
+        return list(Score.select().where(Score.board == scoreboard))
