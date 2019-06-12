48643208a59b5f25cd9f3cee23df487b7b054364
Philipp Krause <phil.fhs11@googlemail.com>
2019-06-12 22:22:52 +0200

    ADD: db information on startup

--- src/database.py
+++ src/database.py
@@ -5,12 +5,14 @@ import urllib.parse
 def connect_db():
     if 'HEROKU' not in os.environ:
         db = peewee.SqliteDatabase('WhatTimeIsIt.db')
+        print("Local SQLite DB active")
     else:
         urllib.parse.uses_netloc.append('postgres')
         url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
         db = peewee.PostgresqlDatabase(database=url.path[1:], user=url.username,
                                        password=url.password, host=url.hostname,
                                        port=url.port, sslmode="require")
+        print("Heroku Postgres DB active")
     return db
 
 def init():
