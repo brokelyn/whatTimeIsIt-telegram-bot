0457b88a5a3c795373e6e6e65ee6160686456eb5
Philipp Krause <phil.fhs11@googlemail.com>
2019-06-17 18:54:29 +0200

    ADD: less than method for score entity

--- src/entity/score.py
+++ src/entity/score.py
@@ -10,3 +10,8 @@ class Score(BaseEntity):
     points = peewee.IntegerField(default=0)
     stat = peewee.ForeignKeyField(model=Statistic)
     date = peewee.IntegerField(default=0)
+
+    def __lt__(self, other):
+        if self.points < other.points:
+            return True
+        return False
