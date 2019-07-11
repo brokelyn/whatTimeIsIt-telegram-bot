77c09fbcd501f0f47a706002f42403723fa20cae
Philipp Krause <phil.fhs11@googlemail.com>
2019-07-11 18:14:46 +0200

    REMOVE: wrong information about events

--- src/controller/event_controller.py
+++ src/controller/event_controller.py
@@ -81,7 +81,6 @@ class EventController:
         reply = "*This events are active:*\n\n"
         for job in active_jobs:
             reply += "Event @ " + job.name + "\n"
-        reply += "\nSever restart will reset all events."
 
         context.bot.send_message(chat_id=update.message.chat_id,
                                  text=reply, parse_mode=telegram.ParseMode.MARKDOWN)
