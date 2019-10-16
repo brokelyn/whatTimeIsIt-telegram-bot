0b5f4048af9e11f0855e5c3995eeaf8eae04be98
Philipp Krause <phil.fhs11@googlemail.com>
2019-10-16 10:29:54 +0200

    UPDATE: chat id fields to big interger

--- src/entity/user.py
+++ src/entity/user.py
@@ -4,7 +4,7 @@ from entity.base_entity import BaseEntity
 
 
 class User(BaseEntity):
-    id = peewee.IntegerField(primary_key=True)
+    id = peewee.BigIntegerField(primary_key=True)
     username = peewee.CharField(255, null=True)
     first_name = peewee.CharField(255)
     last_name = peewee.CharField(255, null=True)
