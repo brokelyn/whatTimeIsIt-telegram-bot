9d4f8f38b85f297e30d3615039fd3408049673ee
Philipp Krause <phil.fhs11@googlemail.com>
2020-03-20 11:09:21 +0100

    FIX: db crash because of no autorollback

--- src/database.py
+++ src/database.py
@@ -1,30 +1,29 @@
 import peewee, os
 import urllib.parse
 
-
 def connect_db():
     if 'HEROKU' not in os.environ:
-        db = peewee.SqliteDatabase('WhatTimeIsIt.db')
+        db = peewee.SqliteDatabase('WhatTimeIsIt.db', autorollback=True, autocommit=True)
         print("Local SQLite DB active")
     else:
         urllib.parse.uses_netloc.append('postgres')
         url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
         db = peewee.PostgresqlDatabase(database=url.path[1:], user=url.username,
                                        password=url.password, host=url.hostname,
-                                       port=url.port, sslmode="require")
+                                       port=url.port, sslmode="require",
+                                       autorollback=True, autocommit=True)
         print("Heroku Postgres DB active")
     return db
 
 
 def init():
-    db = connect_db()
-
     from entity.message import Message
     from entity.user import User
     from entity.statistic import Statistic
     from entity.score import Score
     from entity.event import Event
 
-    db.create_tables([User, Message, Score, Statistic, Event])
+    connection.create_tables([User, Message, Score, Statistic, Event])
 
-    db.close()
+
+connection = connect_db()
