422dd23d9e46394c9107c8a8177eaa807e785d69
brokelyn <phil.fhs11@googlemail.com>
2020-12-11 15:06:22 +0100

    ADD: new entity group in order to support multiple groups at once

---
+++ src/repo/group_repo.py
@@ -0,0 +1,26 @@
+from entity.group import Group
+
+
+class GroupRepo:
+
+    @staticmethod
+    def create(group: Group):
+        group.save(force_insert=True)
+
+    @staticmethod
+    def save(group: Group):
+        group.save()
+
+    @staticmethod
+    def create_if_not_exist(group: Group):
+        query = Group.select().where(Group.id == group.id)
+        if not query.exists():
+            GroupRepo.create(group)
+
+    @staticmethod
+    def get_or_none(id: int) -> Group:
+        return Group.get_or_none(Group.id == id)
+
+    @staticmethod
+    def findAll():
+        return list(Group.select())
