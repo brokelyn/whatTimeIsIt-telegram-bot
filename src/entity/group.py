41e05529e610e1e54c705c80f0beb979bb3defd9
brokelyn <phil.fhs11@googlemail.com>
2020-12-13 11:51:32 +0100

    ADD: auto kicking members which post a wrong time

--- src/entity/group.py
+++ src/entity/group.py
@@ -6,4 +6,7 @@ from entity.base_entity import BaseEntity
 class Group(BaseEntity):
     id = peewee.BigIntegerField(primary_key=True)
     title = peewee.TextField()
+    auto_events = peewee.BooleanField(default=False)
+    auto_ban = peewee.BooleanField(default=False)
+    timezone = peewee.TextField(null=True)
     invite_link = peewee.TextField(null=True)
