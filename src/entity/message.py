da48fe2594e62ca6909221ff715e4a3b05197a1d
brokelyn <phil.fhs11@googlemail.com>
2020-12-27 14:34:13 +0100

    REMOVE: unused database field chat_id in message tabel

--- src/entity/message.py
+++ src/entity/message.py
@@ -9,7 +9,6 @@ class Message(BaseEntity):
     id = peewee.AutoField(primary_key=True)
     msg_id = peewee.BigIntegerField()
     text = peewee.TextField()
-    chat_id = peewee.BigIntegerField()
     user = peewee.ForeignKeyField(model=User)
     time = peewee.DateTimeField()
     group = peewee.ForeignKeyField(model=Group)
